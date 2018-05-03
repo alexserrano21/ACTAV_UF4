692e6d5b2dc8a2bc1cc701d3c99717308117337c
alexserranoescribano <alexserranoescribano@172.26.0.237>
2018-05-03 10:19:40 +0200

    añado metodo correr a la clase animal

---
+++ src/animals/Animal.java
@@ -0,0 +1,30 @@
+package animals;
+
+public abstract class Animal {
+
+    private String nombre;
+
+    public Animal() {
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void comer() {
+        System.out.println(this.getNombre() + " está comiendo...");
+    }
+
+    public void caminar() {
+        System.out.println(this.getNombre() + " está caminando...");
+    }
+    public void correr() {
+        System.out.println(this.getNombre() + " está corrent...");
+    }
+
+    public abstract void sonido();
+}
